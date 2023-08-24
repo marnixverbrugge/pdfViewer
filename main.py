e3e5dcdb2a90b6db8bea3414ae7a78b1361925a1
mjverbrugge <marnix.verbrugge@hotmail.nl>
2023-08-24 14:20:50 +0200

    Initial window

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 """
 Main file
+
+
+Create a tkinter window to show the first page of a pdf document
 """
 
+from tkinter import *
+from tkinter import filedialog
+from tkPDFViewer import tkPDFViewer as pdf
+import os
 
-print('test')
+# Create main window
+root = Tk()
+root.geometry('400x400')
+root.title('PDF page viewer')
+
+
+# Create open button
+def openFile():
+    """Function to open a selected pdf file"""
+
+    fileName = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                          title='select pdf file',
+                                          filetype = (('PDF File', '.pdf'),
+                                                      ('PDF File', '.PDF'),
+                                                      ('ALL file', '.txt')))
+
+    if fileName:
+        print(fileName)
+    else:
+        print('No file selected')
+
+Button(root, text='open', command=openFile, width=60).pack()
+
+# Run
+root.mainloop()
